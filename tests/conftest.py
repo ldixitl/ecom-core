bfc82de668946e53e42ac51cfc98cf0df1755d0e
Nikolay Gendin <gendin786@gmail.com>
2025-03-03 14:33:39 +0300

    added new tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import json
 
 import pytest
 
+from src.models import Category, Product
+
 
 @pytest.fixture
 def sample_json(tmp_path):
@@ -20,3 +22,33 @@ def sample_json(tmp_path):
     with open(file_path, "w", encoding="utf-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
     return file_path
+
+
+@pytest.fixture
+def first_product() -> Product:
+    """Фикстура для первого продукта."""
+    return Product("Samsung Galaxy", "256GB, Серый цвет", 100000.0, 10)
+
+
+@pytest.fixture
+def second_product() -> Product:
+    """Фикстура для второго продукта."""
+    return Product("iPhone 15", "512GB, Gray space", 200000.0, 5)
+
+
+@pytest.fixture
+def product_list(first_product, second_product) -> list[Product]:
+    """Фикстура для списка продуктов."""
+    return [first_product, second_product]
+
+
+@pytest.fixture
+def category_with_products(product_list) -> Category:
+    """Фикстура для категории с товарами."""
+    return Category("Смартфоны", "Описание смартфонов", product_list)
+
+
+@pytest.fixture
+def empty_category() -> Category:
+    """Фикстура для категории без товаров."""
+    return Category("Смартфоны", "Описание смартфонов")
